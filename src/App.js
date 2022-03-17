aebe05325c991dcaf773b34b88204df8bc8bb04a
Ewooral <boahen.cos@gmail.com>
2022-03-17 11:01:44 +0000

    algoewooral admin challenge

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// CSS MODULES
+import './App.css';
+import './index.css';
+
+const App = () => {
+    return (
+        <main className="App"> 
+            <h1>Hello World</h1>
+        </main>
+    )
+}
+
+export default App
