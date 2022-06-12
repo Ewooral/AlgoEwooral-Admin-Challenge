e62786f9bf1ef24a6725207a5a6713c4822fb4de
Ewooral <boahen.cos@gmail.com>
2022-06-12 23:34:50 +0000

    React admin dashboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from 'react'
-
-// CSS MODULES
-import './App.css';
-import './index.css';
+import "./App.css"
+import React, { Component } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {FiSettings} from 'react-icons/fi'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 const App = () => {
-    return (
-        <main className="App"> 
-            <h1>Hello World</h1>
-        </main>
-    )
+  return (
+    <div>
+        <BrowserRouter>
+            <div className="flex relative dark:bg-main-dark-bg">
+                <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
+                    <TooltipComponent content="settings" position="Top">
+                        <button>
+                            <FiSettings />
+                        </button>
+                    </TooltipComponent>
+                </div>
+
+                {activeMenu ? 
+                (
+                    <div className="w-72 fixed sidebar dark:big-secondary-dark-big bg-white">
+                        Sidebar
+                    </div>
+                ) : 
+                
+                (
+                    <div>
+                        Sidebar w-0
+                    </div>
+                )}
+            </div>
+
+        </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
